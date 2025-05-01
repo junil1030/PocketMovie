d6af48fc018345b27da6d321867ca5778612fd7c
JunilSeo <dccrdseo@naver.com>
2025-05-01 21:14:04 +0900

    feat: 영화진흥위원회 API로 부터 받아올 DataModel 생성

---
+++ PocketMovie/Source/Domain/Entities/KOBISMovieSearchResponse.swift
@@ -0,0 +1,27 @@
+//
+//  KOBISMovieSearchResponse.swift
+//  PocketMovie
+//
+//  Created by 서준일 on 5/1/25.
+//
+
+import Foundation
+
+struct KOBISMovieSearchResponse: Codable {
+    let movieListResult: MovieListResult
+}
+
+struct MovieListResult: Codable {
+    let totCnt: Int
+    let source: String
+    let movieList: [KOBISMovie]
+}
+
+struct KOBISMovie: Codable, Identifiable {
+    let movieCd: String
+    let movieNm: String
+    let prdtYear: String
+    let openDt: String
+    
+    var id: String { movieCd }
+}
