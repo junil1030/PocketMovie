6e519eb76d7caff7d8a064ca52691d5dcd2349f2
JunilSeo <dccrdseo@naver.com>
2025-05-01 21:15:59 +0900

    feat: API Service Protocol을 만들고  요청을 보낼 클래스 생성

---
+++ PocketMovie/Source/Data/Network/Services/MovieAPIService.swift
@@ -0,0 +1,44 @@
+//
+//  MovieAPIService.swift
+//  PocketMovie
+//
+//  Created by 서준일 on 5/1/25.
+//
+
+import Foundation
+import Combine
+
+protocol MovieAPIService {
+    func fetchDailyBoxOffice(date: String) -> AnyPublisher<DailyBoxOfficeResponse, Error>
+    func fetchWeeklyBoxOffice(date: String, weekGb: String) -> AnyPublisher<WeeklyBoxOfficeResponse, Error>
+    func searchMovies(keyword: String) -> AnyPublisher<KOBISMovieSearchResponse, Error>
+    func fetchMovieDetail(movieCd: String) -> AnyPublisher<KMDBMovieResponse, Error>
+}
+
+final class DefaultMovieAPIService: MovieAPIService {
+    private let networkClient: NetworkClient
+    
+    init(networkClient: NetworkClient) {
+        self.networkClient = networkClient
+    }
+    
+    func fetchDailyBoxOffice(date: String) -> AnyPublisher<DailyBoxOfficeResponse, Error> {
+        let endpoint = KOBISAPIEndpoint.dailyBoxOffice(targetData: date)
+        return networkClient.request(endpoint)
+    }
+    
+    func fetchWeeklyBoxOffice(date: String, weekGb: String = "0") -> AnyPublisher<WeeklyBoxOfficeResponse, Error> {
+        let endpoint = KOBISAPIEndpoint.weeklyBoxOffice(targetDate: date, weekGb: weekGb)
+        return networkClient.request(endpoint)
+    }
+    
+    func searchMovies(keyword: String) -> AnyPublisher<KOBISMovieSearchResponse, Error> {
+        let endpoint = KOBISAPIEndpoint.searchMovies(keyword: keyword)
+        return networkClient.request(endpoint)
+    }
+    
+    func fetchMovieDetail(movieCd: String) -> AnyPublisher<KMDBMovieResponse, Error> {
+        let endpoint = KMDBAPIEndpoint.movieDetail(movieCd: movieCd)
+        return networkClient.request(endpoint)
+    }
+}
