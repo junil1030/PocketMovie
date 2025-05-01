d8ec213872ec47bae9369f98187b77402e403e24
JunilSeo <dccrdseo@naver.com>
2025-05-01 17:10:47 +0900

    feat: 저장할 Data에 대한 Model 생성

---
+++ PocketMovie/Source/Domain/Entities/Movie.swift
@@ -0,0 +1,39 @@
+//
+//  Movie.swift
+//  PocketMovie
+//
+//  Created by 서준일 on 5/1/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Movie {
+    var title: String
+    var movieCode: String
+    var releaseDate: String
+    var posterURL: String?
+    
+    var rating: Double
+    var review: String
+    var watchedDate: Date
+    
+    init(
+        title: String,
+        movieCode: String,
+        releaseDate: String,
+        posterURL: String? = nil,
+        rating: Double,
+        review: String,
+        watchedDate: Date
+    ) {
+        self.title = title
+        self.movieCode = movieCode
+        self.releaseDate = releaseDate
+        self.posterURL = posterURL
+        self.rating = rating
+        self.review = review
+        self.watchedDate = watchedDate
+    }
+}
