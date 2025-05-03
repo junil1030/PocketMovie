d923f114e42ab81908489d660dbb253b4db731a2
JunilSeo <dccrdseo@naver.com>
2025-05-03 18:25:44 +0900

    feat: Data 저장을 위한 CRUD Repository 생성

---
+++ PocketMovie/Source/Domain/Repositories/MovieRepository.swift
@@ -0,0 +1,22 @@
+//
+//  MovieRepository.swift
+//  PocketMovie
+//
+//  Created by 서준일 on 5/3/25.
+//
+
+import Foundation
+import SwiftData
+
+protocol MovieRepository {
+    func getAllMovies() async throws -> [Movie]
+    func getMovie(byId id: PersistentIdentifier) async throws -> Movie?
+    func saveMovie(_ movie: Movie) async throws
+    func updateMovie(_ movie: Movie) async throws
+    func deleteMovie(_ movie: Movie) async throws
+    func deleteMovies(_ movie: [Movie]) async throws
+    func deleteAllMovies() async throws
+    
+    @MainActor
+    func getModelContext() -> ModelContext
+}
