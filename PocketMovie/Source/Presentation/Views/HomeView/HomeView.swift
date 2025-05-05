62465122ee56ff0b131fdcb99cd253680602ea9a
JunilSeo <dccrdseo@naver.com>
2025-05-05 17:53:27 +0900

    feat: HomeView 생성

---
+++ PocketMovie/Source/Presentation/Views/HomeView/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  PocketMovie
+//
+//  Created by 서준일 on 5/6/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
