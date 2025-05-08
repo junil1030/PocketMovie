3ff637fe82414a3d00bde8ec743ba26e362f6a60
JunilSeo <dccrdseo@naver.com>
2025-05-08 14:32:58 +0900

    feat: SettingsView 생성

---
+++ PocketMovie/Source/Presentation/Views/SettingsView/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  PocketMovie
+//
+//  Created by 서준일 on 5/8/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
