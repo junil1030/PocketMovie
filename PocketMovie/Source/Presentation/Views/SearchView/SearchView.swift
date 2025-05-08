ff3e3b2f540b591d1fa0b4fb86d18c935f14dcd6
JunilSeo <dccrdseo@naver.com>
2025-05-08 21:44:02 +0900

    feat: SearchView 구현 및 샘플 데이터로 테스트

--- PocketMovie/Source/Presentation/Views/SearchView/SearchView.swift
+++ PocketMovie/Source/Presentation/Views/SearchView/SearchView.swift
@@ -8,8 +8,123 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State private var searchText: String = ""
+    @State private var progress: CGFloat = 0
+    @FocusState private var isFocused: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.vertical) {
+            LazyVStack(spacing: 15) {
+                ForEach(imageItems) { item in
+                    CardView(item)
+                }
+            }
+            .padding(15)
+            .offset(y: isFocused ? 0 : progress * 75)
+            .safeAreaInset(edge: .top, spacing: 0) {
+                ResizableHeader()
+            }
+            .scrollTargetLayout()
+        }
+        .scrollTargetBehavior(CustomScrollTarget())
+        .animation(.snappy(duration: 0.3, extraBounce: 0), value: isFocused)
+        .onScrollGeometryChange(for: CGFloat.self) {
+            $0.contentOffset.y + $0.contentInsets.top
+        } action: { oldValue, newValue in
+            progress = max(min(newValue / 75, 1), 0)
+        }
+
+    }
+    
+    @ViewBuilder
+    func ResizableHeader() -> some View {
+        let progress = isFocused ? 1 : progress
+        let currentFocus = isFocused
+        
+        VStack(spacing: 0) {
+            HStack {
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("영화")
+                        .font(.title.bold())
+                }
+                Spacer(minLength: 0)
+            }
+            .frame(height: 60 - (60 * progress), alignment: .bottom)
+            .padding(.horizontal, 15)
+            .padding(.top, 15)
+            .padding(.bottom, 15 - (15 * progress))
+            .opacity(1 - progress)
+            .offset(y: -10 * progress)
+            
+            HStack(spacing: 8) {
+                Image(systemName: "magnifyingglass")
+                
+                TextField("Search Movie", text: $searchText)
+                    .focused($isFocused)
+            }
+            .padding(.vertical, 12)
+            .padding(.horizontal, 15)
+            .background {
+                RoundedRectangle(cornerRadius: isFocused ? 0 : 30)
+                    .fill(.background
+                        .shadow(.drop(color: .black.opacity(0.08), radius: 5, x: 5, y: 5))
+                        .shadow(.drop(color: .black.opacity(0.05), radius: 5, x: -5, y: -5))
+                    )
+                    .padding(.top, isFocused ? -100 : 0)
+            }
+            .padding(.horizontal, isFocused ? 0 : 15)
+            .padding(.bottom, 10)
+            .padding(.top, 5)
+        }
+        .background {
+            ProgressiveBlurView()
+                .blur(radius: isFocused ? 0 : 10)
+                .padding(.horizontal, -15)
+                .padding(.bottom, -10)
+                .padding(.top, -100)
+        }
+        .visualEffect { content, proxy in
+            content
+                .offset(y: offsetY(proxy, isFocused: currentFocus))
+        }
+    }
+    
+    nonisolated private
+    func offsetY(_ proxy: GeometryProxy, isFocused: Bool) -> CGFloat {
+        let minY = proxy.frame(in: .scrollView(axis: .vertical)).minY
+        return minY > 0 ? (isFocused ? -minY : 0) : -minY
+    }
+    
+    @ViewBuilder
+    func CardView(_ item: Item) -> some View {
+        VStack(alignment: .leading, spacing: 8) {
+            GeometryReader {
+                let size = $0.size
+                
+                if let image = item.image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: size.width, height: size.height)
+                        .clipShape(.rect(cornerRadius: 20))
+                }
+            }
+            .frame(height: 220)
+        }
+    }
+}
+
+struct CustomScrollTarget: ScrollTargetBehavior {
+    func updateTarget(_ target: inout ScrollTarget, context: TargetContext) {
+        let endPoint = target.rect.minY
+        
+        if endPoint < 75 {
+            if endPoint > 40 {
+                target.rect.origin = .init(x: 0, y: 75)
+            } else {
+                target.rect.origin = .zero
+            }
+        }
     }
 }
 
