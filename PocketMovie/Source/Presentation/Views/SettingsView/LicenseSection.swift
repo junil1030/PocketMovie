ed1ac94d103edd3e07ca359bef9d350b7e0846df
JunilSeo <dccrdseo@naver.com>
2025-05-12 12:17:55 +0900

    feat: 라이센스 섹션 구성

--- PocketMovie/Source/Presentation/Views/SettingsView/LicenseSection.swift
+++ PocketMovie/Source/Presentation/Views/SettingsView/LicenseSection.swift
@@ -8,8 +8,41 @@
 import SwiftUI
 
 struct LicenseSection: View {
+    
+    struct License: Identifiable {
+        var id = UUID()
+        var name: String
+        var url: String
+    }
+    
+    let licenses: [License] = [
+        License(name: "Alamofire", url: "https://github.com/Alamofire/Alamofire"),
+        License(name: "Kingfisher", url: "https://github.com/onevcat/Kingfisher"),
+        License(name: "Swinject", url: "https://github.com/Swinject/Swinject"),
+    ]
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationLink {
+            List(licenses) { library in
+                VStack(alignment: .leading, spacing: 8) {
+                    Text(library.name)
+                        .font(.headline)
+                    
+                    Link(library.url, destination: URL(string: library.url)!)
+                        .font(.caption)
+                        .foregroundColor(.blue)
+                }
+                .padding(.vertical, 4)
+            }
+            .navigationTitle("오픈소스 라이선스")
+            .navigationBarTitleDisplayMode(.inline)
+        } label: {
+            HStack {
+                Image(systemName: "doc.text")
+                Text("오픈소스 라이선스")
+            }
+        }
     }
 }
 
