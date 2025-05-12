74431f4015552a5463dfdc0ea94ed56db5ccf1e3
JunilSeo <dccrdseo@naver.com>
2025-05-12 12:16:02 +0900

    feat: Data 초기화 섹션 구성

---
+++ PocketMovie/Source/Presentation/Views/SettingsView/LicenseSection.swift
@@ -0,0 +1,18 @@
+//
+//  LicenseSection.swift
+//  PocketMovie
+//
+//  Created by 서준일 on 5/12/25.
+//
+
+import SwiftUI
+
+struct LicenseSection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LicenseSection()
+}
---
+++ PocketMovie/Source/Presentation/Views/SettingsView/ReviewSection.swift
@@ -0,0 +1,18 @@
+//
+//  ReviewSection.swift
+//  PocketMovie
+//
+//  Created by 서준일 on 5/12/25.
+//
+
+import SwiftUI
+
+struct ReviewSection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReviewSection()
+}
