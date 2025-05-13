eac4e96e42290845131b2c49cf0d5c1fbb01be54
JunilSeo <dccrdseo@naver.com>
2025-05-13 11:50:39 +0900

    refactor: 카드 없을 때 띄울 빈 화면

---
+++ PocketMovie/Source/Presentation/Views/HomeView/Components/CardStackView.swift
@@ -0,0 +1,18 @@
+//
+//  CardStackView.swift
+//  PocketMovie
+//
+//  Created by 서준일 on 5/13/25.
+//
+
+import SwiftUI
+
+struct CardStackView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardStackView()
+}
---
+++ PocketMovie/Source/Presentation/Views/HomeView/Components/EmptyStateView.swift
@@ -0,0 +1,27 @@
+//
+//  EmptyStateView.swift
+//  PocketMovie
+//
+//  Created by 서준일 on 5/13/25.
+//
+
+import SwiftUI
+
+struct EmptyStateView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "film")
+                .font(.system(size: 70))
+                .foregroundStyle(.gray)
+                .padding()
+            
+            Text("영화를 추가해보세요!")
+                .font(.title2)
+                .foregroundStyle(.gray)
+        }
+    }
+}
+
+#Preview {
+    EmptyStateView()
+}
