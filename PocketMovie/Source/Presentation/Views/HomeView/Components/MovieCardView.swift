cd03f0753f4eccda57190fa927723b159efcdaa7
JunilSeo <dccrdseo@naver.com>
2025-05-13 11:52:17 +0900

    feat: Card 개별 View 분리

--- PocketMovie/Source/Presentation/Views/HomeView/Components/MovieCardView.swift
+++ PocketMovie/Source/Presentation/Views/HomeView/Components/MovieCardView.swift
@@ -8,11 +8,47 @@
 import SwiftUI
 
 struct MovieCardView: View {
+    let movie: Movie
+    let isSelected: Bool
+    let isFlipped: Bool
+    let cardWidth: CGFloat
+    let cardHeight: CGFloat
+    let cardCornerRadius: CGFloat
+    let cardBackgroundCornerRadius: CGFloat
+    let onTap: () -> Void
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            if isFlipped {
+                ReviewCardView(
+                    movie: movie,
+                    cardWidth: cardWidth,
+                    cardHeight: cardHeight,
+                    cardCornerRadius: cardCornerRadius,
+                    cardBackgroundCornerRadius: cardBackgroundCornerRadius
+                )
+            } else {
+                PosterCardView(
+                    movie: movie,
+                    isSelected: isSelected,
+                    cardWidth: cardWidth,
+                    cardHeight: cardHeight,
+                    cardCornerRadius: cardCornerRadius,
+                    cardBackgroundCornerRadius: cardBackgroundCornerRadius
+                )
+            }
+        }
+        .rotation3DEffect(
+            .degrees(isFlipped ? 180 : 0),
+            axis: (x: 0, y: 1, z: 0)
+        )
+        .animation(.spring(), value: isFlipped)
+        .onTapGesture {
+            onTap()
+        }
     }
 }
 
 #Preview {
-    MovieCardView()
+    //MovieCardView()
 }
