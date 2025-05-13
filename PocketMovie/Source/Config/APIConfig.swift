a2e57931a7f10d5106027bbd08442dac7cb17b35
JunilSeo <dccrdseo@naver.com>
2025-05-13 21:07:24 +0900

    chore: ignore 변경

---
+++ PocketMovie/Source/Config/APIConfig.swift
@@ -0,0 +1,42 @@
+//
+//  APIConfig.swift
+//  PocketMovie
+//
+//  Created by 서준일 on 5/1/25.
+//
+
+import Foundation
+
+enum APIConfig {
+    static var kobisAPIKey: String {
+        #if DEBUG
+        if ProcessInfo.processInfo.environment["CI"] == "TRUE" {
+            return ProcessInfo.processInfo.environment["KOBIS_API_KEY"] ?? ""
+        } else {
+            return Bundle.main.infoDictionary?["KOBIS_API_KEY"] as? String ?? ""
+        }
+        #else
+        if ProcessInfo.processInfo.environment["CI"] == "TRUE" {
+            return ProcessInfo.processInfo.environment["KOBIS_API_KEY"] ?? ""
+        } else {
+            return Bundle.main.infoDictionary?["KOBIS_API_KEY"] as? String ?? ""
+        }
+        #endif
+    }
+    
+    static var kmdbAPIKey: String {
+        #if DEBUG
+        if ProcessInfo.processInfo.environment["CI"] == "TRUE" {
+            return ProcessInfo.processInfo.environment["KMDB_API_KEY"] ?? ""
+        } else {
+            return Bundle.main.infoDictionary?["KMDB_API_KEY"] as? String ?? ""
+        }
+        #else
+        if ProcessInfo.processInfo.environment["CI"] == "TRUE" {
+            return ProcessInfo.processInfo.environment["KMDB_API_KEY"] ?? ""
+        } else {
+            return Bundle.main.infoDictionary?["KMDB_API_KEY"] as? String ?? ""
+        }
+        #endif
+    }
+}
