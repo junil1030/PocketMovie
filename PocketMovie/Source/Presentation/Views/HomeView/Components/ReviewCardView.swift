7afa42cb1ded2c4647787c65251e7e8892391240
JunilSeo <dccrdseo@naver.com>
2025-05-13 11:54:02 +0900

    refactor: 카드 뒷면 View 분리

--- PocketMovie/Source/Presentation/Views/HomeView/Components/ReviewCardView.swift
+++ PocketMovie/Source/Presentation/Views/HomeView/Components/ReviewCardView.swift
@@ -8,11 +8,60 @@
 import SwiftUI
 
 struct ReviewCardView: View {
+    let movie: Movie
+    let cardWidth: CGFloat
+    let cardHeight: CGFloat
+    let cardCornerRadius: CGFloat
+    let cardBackgroundCornerRadius: CGFloat
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            // 배경
+            RoundedRectangle(cornerRadius: cardCornerRadius)
+                .fill(Color.black.opacity(0.8))
+                .frame(width: cardWidth, height: cardHeight)
+            
+            // 리뷰 텍스트
+            VStack(spacing: 20) {
+                Text(movie.title)
+                    .font(.headline)
+                    .foregroundColor(.white)
+                
+                Divider()
+                    .background(Color.white)
+                
+                Text("\"" + movie.review + "\"")
+                    .font(.body)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal)
+                
+                Spacer()
+                
+                HStack {
+                    ForEach(1...5, id: \.self) { index in
+                        Image(systemName: index <= Int(movie.rating) ? "star.fill" : "star")
+                            .foregroundColor(.yellow)
+                    }
+                }
+                .padding(.bottom)
+            }
+            .padding()
+            .frame(width: cardWidth, height: cardHeight)
+            .rotation3DEffect(
+                .degrees(180),
+                axis: (x: 0, y: 1, z: 0)
+            )
+        }
+        .clipShape(.rect(cornerRadius: cardCornerRadius))
+        .padding(5)
+        .background {
+            RoundedRectangle(cornerRadius: cardBackgroundCornerRadius)
+                .fill(.background)
+        }
     }
 }
 
 #Preview {
-    ReviewCardView()
+    //ReviewCardView()
 }
