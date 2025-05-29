80d926f53274d33c35add698e440af809018ed38
junil1030 <dccrdseo@naver.com>
2025-05-29 23:56:34 +0900

    feat: TMDB 결과에 대한 Response 작성

---
+++ PocketMovie/Source/Domain/Entities/TMDBMovieResponse.swift
@@ -0,0 +1,55 @@
+//
+//  TMDBMovieResponse.swift
+//  PocketMovie
+//
+//  Created by 서준일 on 5/29/25.
+//
+
+import Foundation
+
+struct TMDBMovieResponse: Codable {
+    let page: Int
+    let results: [TMDBMovie]
+    let totalPages: Int
+    let totalResults: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case page, results
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
+}
+
+struct TMDBMovie: Codable, Identifiable {
+    let id: Int
+    let title: String
+    let originalTitle: String
+    let posterPath: String?
+    let backdropPath: String?
+    let releaseDate: String
+    let overview: String
+    let voteAverage: Double
+    let voteCount: Int
+    
+    // 검색 창에 띄울 포스터 URL
+    var fullPosterURL: String? {
+        guard let posterPath = posterPath else { return nil }
+        return "https://image.tmdb.org/t/p/w342\(posterPath)"
+    }
+    
+    // 디테일 창에 띄울 포스터 URL
+    var largePosterURL: String? {
+        guard let posterPath = posterPath else { return nil }
+        return "https://image.tmdb.org/t/p/w500\(posterPath)"
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case id, title, overview
+        case originalTitle = "original_title"
+        case posterPath = "poster_path"
+        case backdropPath = "backdrop_path"
+        case releaseDate = "release_date"
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+}
