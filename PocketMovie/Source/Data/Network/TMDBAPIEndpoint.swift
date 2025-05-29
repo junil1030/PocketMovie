e98fc00439bf6c68a150ad82373cf8b423c3ff9c
junil1030 <dccrdseo@naver.com>
2025-05-29 23:56:05 +0900

    feat: TMDB API 사용을 위한 Endpoint 작성

---
+++ PocketMovie/Source/Data/Network/TMDBAPIEndpoint.swift
@@ -0,0 +1,54 @@
+//
+//  TMDBAPIEndpoint.swift
+//  PocketMovie
+//
+//  Created by 서준일 on 5/29/25.
+//
+
+import Foundation
+import Alamofire
+
+struct TMDBAPIKeys {
+    static let apiKey = APIConfig.tmdbAPIKey
+}
+
+enum TMDBAPIEndpoint {
+    case searchMovies(keyword: String, page: Int = 1)
+    case movieDetail(movieId: Int)
+}
+
+extension TMDBAPIEndpoint: APIEndpoint {
+    var baseURL: String {
+        return "https://api.themoviedb.org/3"
+    }
+    
+    var path: String {
+        switch self {
+        case .searchMovies:
+            return "/search/movie"
+        case .movieDetail(let movieId):
+            return "/movie/\(movieId)"
+        }
+    }
+    
+    var method: HTTPMethod {
+        return .get
+    }
+    
+    var parameters: Parameters? {
+        var params: Parameters = [
+            "api_key": TMDBAPIKeys.apiKey,
+            "language": "ko-KR"
+        ]
+        
+        switch self {
+        case .searchMovies(let keyword, let page):
+            params["query"] = keyword
+            params["page"] = page
+        case .movieDetail:
+            params["append_to_response"] = "credits, videos, similar"
+        }
+        
+        return params
+    }
+}
